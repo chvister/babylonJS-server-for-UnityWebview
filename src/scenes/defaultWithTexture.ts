bc1d2f61aa47c8280a50e6ae7159e1987d3dfb3e
Martin Svoren <msvoro@gmail.com>
2021-08-09 17:41:17 +0200

    WIP add buttons

--- src/scenes/defaultWithTexture.ts
+++ src/scenes/defaultWithTexture.ts
@@ -1,5 +1,7 @@
 import { CreateSceneClass } from "../createScene";
-import { Engine, Scene, FreeCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, SceneLoader } from "@babylonjs/core";
+import { Engine, Scene, FreeCamera, UniversalCamera, Vector3, HemisphericLight, Mesh, SceneLoader } from "@babylonjs/core";
+import { AdvancedDynamicTexture, Button, InputText } from '@babylonjs/gui'
+
 import "@babylonjs/loaders/glTF/2.0/glTFLoader";
 export class DefaultSceneWithTexture implements CreateSceneClass {
 
@@ -10,19 +12,6 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
         // This creates a basic Babylon Scene object (non-mesh)
         const scene = new Scene(engine);
 
-        const camera = new FreeCamera("FreeCamera", new Vector3(0, 5, -10), scene);
-
-        camera.attachControl(canvas, true);
-        camera.inputs.addMouseWheel();
-
-        camera.setTarget(Vector3.Zero());
-
-        camera.attachControl(canvas, true);
-
-        const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-
-        light.intensity = 0.7;
-
         SceneLoader.ImportMesh(
             "",
             "http://127.0.0.1:5502/",
@@ -32,6 +21,61 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
                 // console.log(m);
             }
         );
+
+        const camera = new FreeCamera("FreeCamera", new Vector3(0, 5, -10), scene);
+
+        camera.attachControl(canvas, true);
+        camera.inputs.addMouseWheel();
+        // camera.speed = 1
+        camera.inertia = 0
+
+        // engine.deltaTime = 0
+
+        // camera.setTarget(Vector3.Zero());
+
+        const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+
+        light.intensity = 0.7;
+
+        var plane = Mesh.CreatePlane("plane", 2, scene);
+        // plane.parent = sphere;
+        plane.position.y = 7;
+        plane.position.z = 5;
+        plane.position.x = 3;
+        var advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane);
+
+        var plane2 = Mesh.CreatePlane("plane2", 2, scene);
+        // plane.parent = sphere;
+        plane2.position.y = 5;
+        plane2.position.z = 5;
+        plane2.position.x = 3;
+        var advancedTexture2 = AdvancedDynamicTexture.CreateForMesh(plane2);
+
+        var button1 = Button.CreateSimpleButton("but1", "Click");
+        button1.width = 0.5;
+        button1.height = 0.4;
+        button1.color = "white";
+        button1.fontSize = 80;
+        button1.background = "gray";
+        button1.onPointerUpObservable.add(function () {
+            button1.background = 'green'
+            button1!.textBlock!.text = 'BED :)'
+        });
+        advancedTexture2.addControl(button1);
+
+        var input = new InputText();
+        input.width = 1;
+        input.maxWidth = 1;
+        input.height = "200px";
+        input.text = "napis odkaz";
+        input.color = "white";
+        input.fontSize = 100;
+        input.background = "gray";
+        advancedTexture.addControl(input);
+
+        // window.addEventListener("resize", () => engine.resize());
+
+        // engine.runRenderLoop(() => scene.render());
 
         return scene;
     };
