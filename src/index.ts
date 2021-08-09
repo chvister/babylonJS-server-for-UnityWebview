6a0b25a4eaefbdf660a45fa4b5a988cbdb432a20
Martin Svoren <msvoro@gmail.com>
2021-08-09 18:30:15 +0200

    WIP fix lagging camera

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,75 @@
-import { Engine } from "@babylonjs/core/Engines/engine";
-import { getSceneModuleWithName } from "./createScene";
+// import { CreateSceneClass } from "../createScene";
+import { Engine, Scene, FreeCamera, UniversalCamera, Vector3, HemisphericLight, Mesh, SceneLoader } from "@babylonjs/core";
+import { AdvancedDynamicTexture, Button, InputText } from '@babylonjs/gui'
 
-const getModuleToLoad = (): string | undefined => location.search.split('scene=')[1];
+import "@babylonjs/loaders/glTF/2.0/glTFLoader";
 
-export const babylonInit = async (): Promise<void>  => {
-    // get the module to load
-    const moduleName = getModuleToLoad();
-    const createSceneModule = await getSceneModuleWithName(moduleName);
+// Basic setup
+const canvas = document.querySelector("#app");
+//@ts-ignore
+const engine = new Engine(canvas, true, null, true);
+const scene = new Scene(engine);
 
-    // Execute the pretasks, if defined
-    await Promise.all(createSceneModule.preTasks || []);
-    // Get the canvas element
-    const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; 
-    // Generate the BABYLON 3D engine
-    const engine = new Engine(canvas, true); 
+const camera = new FreeCamera("FreeCamera", new Vector3(0, 5, -10),scene);
 
-    // Create the scene
-    const scene = await createSceneModule.createScene(engine, canvas);
+camera.attachControl(canvas, true);
+camera.inputs.addMouseWheel();
 
-    // Register a render loop to repeatedly render the scene
-    engine.runRenderLoop(function () {
-        scene.render();
-    });
+// camera.setTarget(Zero());
 
-    // Watch for browser/canvas resize events
-    window.addEventListener("resize", function () {
-        engine.resize();
-    });
-}
+//@ts-ignore
+scene.activeCamera.attachControl(canvas, true);
 
-babylonInit().then(() => {
-    // scene started rendering, everything is initialized
-});
+const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+light.intensity = 0.7;
+
+window.addEventListener("resize", () => engine.resize());
+
+engine.runRenderLoop(() => scene.render());
+
+// var plane =  Mesh.CreatePlane("plane", 2);
+// // plane.parent = sphere;
+// plane.position.y = 7;
+// plane.position.z = 5;
+// plane.position.x = 3;
+// var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane);
+
+// var plane2 = BABYLON.Mesh.CreatePlane("plane2", 2);
+// // plane.parent = sphere;
+// plane2.position.y = 5;
+// plane2.position.z = 5;
+// plane2.position.x = 3;
+// var advancedTexture2 = BABYLON.GUI.AdvancedDynamicTexture.CreateForMesh(plane2);
+
+// var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Click");
+// button1.width = 0.5;
+// button1.height = 0.4;
+// button1.color = "white";
+// button1.fontSize = 80;
+// button1.background = "gray";
+// button1.onPointerUpObservable.add(function () {
+//     // alert("this is bed");
+//     button1.background = 'green'
+//     button1.textBlock.text = 'BED :)'
+// });
+// advancedTexture2.addControl(button1);
+
+// var input = new BABYLON.GUI.InputText();
+// input.width = 1;
+// input.maxWidth = 1;
+// input.height = "200px";
+// input.text = "napis odkaz";
+// input.color = "white";
+// input.fontSize = 100;
+// input.background = "gray";
+// advancedTexture.addControl(input);
+
+SceneLoader.ImportMesh(
+  "",
+  "http://127.0.0.1:5502/",
+  "scenes/interior/scene.glb",
+  scene,
+  function (m) {
+    // console.log(m);
+  }
+);
