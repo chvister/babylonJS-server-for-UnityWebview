085d0d2ff8d35658df589d0d5376f0f7ae7b740e
Martin Svoren <msvoro@gmail.com>
2021-08-09 15:27:26 +0200

    WIP load model

--- src/scenes/defaultWithTexture.ts
+++ src/scenes/defaultWithTexture.ts
@@ -1,15 +1,6 @@
-import { SphereBuilder } from "@babylonjs/core/Meshes/Builders/sphereBuilder";
-import { GroundBuilder } from "@babylonjs/core/Meshes/Builders/groundBuilder";
-import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
 import { CreateSceneClass } from "../createScene";
-import { Engine, Scene, ArcRotateCamera, FreeCamera, Vector3, HemisphericLight, Mesh, MeshBuilder } from "@babylonjs/core";
-
-// If you don't need the standard material you will still need to import it since the scene requires it.
-// import "@babylonjs/core/Materials/standardMaterial";
-import { Texture } from "@babylonjs/core/Materials/Textures/texture";
-
-import grassTextureUrl from "../../assets/grass.jpg";
-
+import { Engine, Scene, FreeCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, SceneLoader } from "@babylonjs/core";
+import "@babylonjs/loaders/glTF/2.0/glTFLoader";
 export class DefaultSceneWithTexture implements CreateSceneClass {
 
     createScene = async (
@@ -19,55 +10,28 @@ export class DefaultSceneWithTexture implements CreateSceneClass {
         // This creates a basic Babylon Scene object (non-mesh)
         const scene = new Scene(engine);
 
-        // This creates and positions a free camera (non-mesh)
-        // const camera = new ArcRotateCamera(
-        //     "my first camera",
-        //     0,
-        //     Math.PI / 3,
-        //     10,
-        //     new Vector3(0, 0, 0),
-        //     scene
-        // );
-
         const camera = new FreeCamera("FreeCamera", new Vector3(0, 5, -10), scene);
 
         camera.attachControl(canvas, true);
         camera.inputs.addMouseWheel();
 
-        // This targets the camera to scene origin
         camera.setTarget(Vector3.Zero());
 
-        // This attaches the camera to the canvas
         camera.attachControl(canvas, true);
 
-        // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
         const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 
-        // Default intensity is 1. Let's dim the light a small amount
-        light.intensity = 0.4;
+        light.intensity = 0.7;
 
-        //Our built-in 'sphere' shape.
-        const sphere = SphereBuilder.CreateSphere(
-            "sphere",
-            { diameter: 2, segments: 32 },
-            scene
+        SceneLoader.ImportMesh(
+            "",
+            "http://127.0.0.1:5502/",
+            "scenes/interior/scene.glb",
+            scene,
+            function (m) {
+                console.log(m);
+            }
         );
-
-        // Move the sphere upward 1/2 its height
-        sphere.position.y = 1;
-
-        // Our built-in 'ground' shape.
-        const ground = GroundBuilder.CreateGround(
-            "ground",
-            { width: 6, height: 6 },
-            scene
-        );
-
-        // Load a texture to be used as the ground material
-        const groundMaterial = new StandardMaterial("ground material", scene);
-        groundMaterial.diffuseTexture = new Texture(grassTextureUrl, scene);
-
-        ground.material = groundMaterial;
 
         return scene;
     };
