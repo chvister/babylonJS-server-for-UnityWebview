2bfe5a21e8c84523c2da11731de6129a13e8f4c6
Martin Svoren <msvoro@gmail.com>
2021-08-10 16:32:46 +0200

    WIP add more roomDatas commented

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // import { CreateSceneClass } from "../createScene";
-import { Engine, Scene, FreeCamera, UniversalCamera, Vector3, HemisphericLight, Mesh, SceneLoader, StandardMaterial, Color3, CircleEase, EasingFunction } from "@babylonjs/core";
+import { Engine, Scene, FreeCamera, UniversalCamera, Vector3, HemisphericLight, Mesh, MeshBuilder, SceneLoader, StandardMaterial, Color3, CircleEase, EasingFunction } from "@babylonjs/core";
 import { AdvancedDynamicTexture, Button, InputText } from '@babylonjs/gui'
 
 import "@babylonjs/loaders/glTF/2.0/glTFLoader";
@@ -24,11 +24,13 @@ scene.activeCamera.attachControl(canvas, true);
 const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
 light.intensity = 0.7;
 
+const box = MeshBuilder.CreateBox("box", { height: 1, width: 1, depth: 1 }, scene)
+//custom
 const tvPlane = Mesh.CreatePlane("plane", 2, scene);
 
 tvPlane.position.x = 2.398400068283081;
 tvPlane.position.y = 6.241600036621094;
-tvPlane.position.z = -11.3681001663208 + 1;
+tvPlane.position.z = -11.3681001663208 + 0.5;//worldmin - worldmax
 tvPlane.billboardMode = Mesh.BILLBOARDMODE_ALL;
 // tvPlane.parent
 
@@ -44,6 +46,7 @@ inputTv.fontSize = 100;
 inputTv.background = "transparent";
 tvTexture.addControl(inputTv);
 
+//custom
 const lampPlane = Mesh.CreatePlane("lampPlane", 2, scene);
 // plane.parent = sphere;
 lampPlane.position.x = 8.346599578857422;
@@ -84,7 +87,7 @@ ground.checkCollisions = true;
 
 window.addEventListener("resize", () => engine.resize());
 engine.runRenderLoop(() => scene.render());
-let roomData
+
 const roomModel = SceneLoader.ImportMesh(
     "",
     "http://127.0.0.1:5502/",
@@ -97,18 +100,18 @@ const roomModel = SceneLoader.ImportMesh(
         //@ts-ignore
         console.log(m[0]._children[10], 'm[0]._children[lamp]');
         //@ts-ignore
-        roomData = m[0]._children
+        const roomData = m[0]._children
         console.log(roomData, 'roomData');
         for (let i = 0; i < roomData.length; i++) {
             const lampPlane = Mesh.CreatePlane(roomData[i].name, 2, scene);
             // plane.parent = sphere;
             lampPlane.position.x = roomData[i]._absolutePosition._x;
-            lampPlane.position.y = roomData[i]._absolutePosition._y + 4;
+            lampPlane.position.y = roomData[i]._absolutePosition._y + 3.5;
             lampPlane.position.z = roomData[i]._absolutePosition._z;
             lampPlane.billboardMode = Mesh.BILLBOARDMODE_ALL;
             //@ts-ignore
             const lampTexture = AdvancedDynamicTexture.CreateForMesh(lampPlane);
-            
+
             const inputLamp = new InputText();
             inputLamp.width = 1;
             inputLamp.maxWidth = 1;
@@ -119,8 +122,37 @@ const roomModel = SceneLoader.ImportMesh(
             inputLamp.background = "transparent";
             lampTexture.addControl(inputLamp);
         }
-        roomData[8]._absolutePosition._x = 6
-        console.log(roomData[8],'roomData[8]._absolutePosition._x'); 
+
+        //more datas
+        const roomData2 = m
+        // console.log(roomData2,'roomData2'); 
+
+        // for (let i = 0; i < roomData2.length; i++) {
+        //     const lampPlane = Mesh.CreatePlane(roomData2[i].name, 2, scene);
+        //     // plane.parent = sphere;
+        //                 //@ts-ignore
+        //     lampPlane.position.x = roomData2[i]._absolutePosition._x;
+        //                 //@ts-ignore
+        //     lampPlane.position.y = roomData2[i]._absolutePosition._y;
+        //                 //@ts-ignore
+        //     lampPlane.position.z = roomData2[i]._absolutePosition._z;
+        //     lampPlane.billboardMode = Mesh.BILLBOARDMODE_ALL;
+        //     //@ts-ignore
+        //     const lampTexture = AdvancedDynamicTexture.CreateForMesh(lampPlane);
+
+        //     const inputLamp = new InputText();
+        //     inputLamp.width = 1;
+        //     inputLamp.maxWidth = 1;
+        //     inputLamp.height = "500px";
+        //     inputLamp.text = roomData2[i].name;
+        //     inputLamp.color = "red";
+        //     inputLamp.fontSize = 100;
+        //     inputLamp.background = "transparent";
+        //     lampTexture.addControl(inputLamp);
+        // }
+        //
+        // roomData[8]._absolutePosition._x = 6
+        // console.log(roomData[8],'roomData[8]._absolutePosition._x'); 
     }
 );
 
