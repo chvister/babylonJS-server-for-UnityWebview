92254cbc52e5d18931d270897b11d4ece7eb139a
Martin Svoren <msvoro@gmail.com>
2021-08-17 11:01:38 +0200

    refaktoring

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {
     Engine,
     Scene,
     SceneLoader
-} from "@babylonjs/core";
+} from "@babylonjs/core"
 import {
     AdvancedDynamicTexture
 } from '@babylonjs/gui'
@@ -12,29 +12,29 @@ import { groundSettings } from "./sceneSettings/ground"
 import { lightSettings } from "./sceneSettings/light"
 import { modelLabelOnClick, modelLabelOnClickXr } from "./sceneSettings/modelLabel"
 
-import "@babylonjs/loaders/glTF/2.0/glTFLoader";
+import "@babylonjs/loaders/glTF/2.0/glTFLoader"
 
-const canvas = document.querySelector("#renderCanvas") as HTMLCanvasElement;
-const engine = new Engine(canvas, true, undefined, true);
-const scene = new Scene(engine);
+const canvas = document.querySelector("#renderCanvas") as HTMLCanvasElement
+const engine = new Engine(canvas, true, undefined, true)
+const scene = new Scene(engine)
 
 cameraSettings(scene, canvas)
 lightSettings(scene)
 groundSettings(scene)
 
-window.addEventListener("resize", () => engine.resize());
-engine.runRenderLoop(() => scene.render());
+window.addEventListener("resize", () => engine.resize())
+engine.runRenderLoop(() => scene.render())
 
 //add xr
-const env = scene.createDefaultEnvironment();
+const env = scene.createDefaultEnvironment()
 
 //@ts-ignore TODO
 const xr = await scene.createDefaultXRExperienceAsync({
     // xrInput: defaultXRExperience.input,  
     // floorMeshes: [environment?.ground] /* Array of meshes to be used as landing points */
-});
+})
 
-setupCameraForCollisions(xr.input.xrCamera);
+setupCameraForCollisions(xr.input.xrCamera)
 
 const roomModel = SceneLoader.ImportMesh(
     "",
@@ -42,20 +42,16 @@ const roomModel = SceneLoader.ImportMesh(
     "scene.glb",
     scene,
     function (mesh) {
-        console.log(mesh);
-        console.log(mesh, 'mesh');
-
+        console.log(mesh, 'mesh')
         const modelTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI")
-        modelLabelOnClick(mesh, scene, modelTexture);
-
+        modelLabelOnClick(mesh, scene, modelTexture)
         modelLabelOnClickXr(mesh, scene, xr, modelTexture)
   
         //TODO remove rotate lamp
-        const lamp = mesh[38];
-        lamp.rotationQuaternion = null;
-
+        const lamp = mesh[38]
+        lamp.rotationQuaternion = null
         scene.beforeRender = () => {
-            lamp.rotation.y += 0.01;
-        };
+            lamp.rotation.y += 0.01
+        }
     }
-);
+)
--- src/sceneSettings/modelLabel.ts
+++ src/sceneSettings/modelLabel.ts
@@ -4,7 +4,7 @@ import {
     ActionManager,
     ExecuteCodeAction,
     WebXRDefaultExperience
-} from "@babylonjs/core";
+} from "@babylonjs/core"
 
 import {
     AdvancedDynamicTexture, Rectangle,
@@ -15,148 +15,134 @@ import {
 
 export const modelLabelOnClick = (mesh: AbstractMesh[], scene: Scene, modelTexture: AdvancedDynamicTexture) => {
     const roomFullData = mesh
-
     for (let i = 0; i < roomFullData.length; i++) {
-        roomFullData[i].actionManager = new ActionManager(scene);
+        roomFullData[i].actionManager = new ActionManager(scene)
         roomFullData[i]?.actionManager?.registerAction(new ExecuteCodeAction(ActionManager.OnDoublePickTrigger, function () {
-            console.log(roomFullData[i].name);
+            console.log(roomFullData[i].name)
             modelLabel(roomFullData[i], modelTexture)
-        }));
+        }))
 
-        if (roomFullData[i].name === 'windowGlass' || roomFullData[i].name === 'featureWall' || roomFullData[i].name === 'Walls') {
-            roomFullData[i].checkCollisions = true;
-        }
+        avoidModelsCollisions(roomFullData[i])
     }
 }
 
-// export const roomData = (mesh: any, scene: any) => {
-//     const roomFullData = mesh
-//     const actualModel = { model: undefined }
-//     for (let i = 0; i < roomFullData.length; i++) {
-//         roomFullData[i].actionManager = new ActionManager(scene);
-//         roomFullData[i]?.actionManager?.registerAction(new ExecuteCodeAction(ActionManager.OnPointerOverTrigger, function () {
-//             // roomFullData[i].actionManager.registerAction(new ExecuteCodeAction(ActionManager.OnPickUpTrigger, function () {
-//             console.log(roomFullData[i].name);
-//             // actualModelClick = roomFullData[i]
-//             // console.log(roomFullData[i]);
-//             // return roomFullData[i]
-//             // return "a"
-//             actualModel.model = roomFullData[i]
-//         }));
-//         return actualModel.model
-//     }
-// }
-
-const textSquare = new Rectangle();
-const labelModel = new TextBlock();
-const targetModel = new Ellipse();
-const lineModel = new Line();
-
-export const modelLabel = (model: AbstractMesh, modelTexture: AdvancedDynamicTexture) => {
-    console.log(model, 'model');
-    labelTextSquare(model, modelTexture)
-    labelTargetModel(model, modelTexture)
-    labelLineModel(model, modelTexture)
-}
-
-const labelTextSquare = (model: AbstractMesh, modelTexture: AdvancedDynamicTexture) => {
-    modelTexture.idealWidth = 600;
-    textSquare.width = 0.14;
-    textSquare.height = "20px";
-    textSquare.cornerRadius = 20;
-    textSquare.color = "Orange";
-    textSquare.thickness = 4;
-    textSquare.background = "green";
-    modelTexture.addControl(textSquare);
-    textSquare.linkWithMesh(model);
-    textSquare.linkOffsetY = -50;
-    if (model) {
-        labelModel.text = model?.name;
+const avoidModelsCollisions = (mesh: AbstractMesh) => {
+    if (mesh.name === 'windowGlass' || mesh.name === 'featureWall' || mesh.name === 'Walls') {
+        mesh.checkCollisions = true
     }
-    textSquare.addControl(labelModel);
-}
-
-const labelTargetModel = (model: AbstractMesh, modelTexture: AdvancedDynamicTexture) => {
-    targetModel.width = "7px";
-    targetModel.height = "7px";
-    targetModel.color = "Orange";
-    targetModel.thickness = 2;
-    targetModel.background = "green";
-    modelTexture.addControl(targetModel);
-    targetModel.linkWithMesh(model);
-}
-
-const labelLineModel = (model: AbstractMesh, modelTexture: AdvancedDynamicTexture) => {
-    lineModel.lineWidth = 4;
-    lineModel.color = "Orange";
-    lineModel.y2 = 10;
-    lineModel.linkOffsetY = -3;
-    modelTexture.addControl(lineModel);
-    lineModel.linkWithMesh(model);
-    lineModel.connectedControl = textSquare;
 }
 
 export const modelLabelOnClickXr = (mesh: AbstractMesh[], scene: Scene, xr: WebXRDefaultExperience, modelTexture: AdvancedDynamicTexture) => {
     const roomFullData = mesh
     xr.input.onControllerAddedObservable.add((controller) => {
-        console.log(xr.input, 'xr.input');
+        console.log(xr.input, 'xr.input')
         controller.onMotionControllerInitObservable.add((motionController) => {
-            console.log(motionController, 'motionController');
-            console.log(controller, 'controller');
-
             if (motionController.handness === 'right') {
-                const xr_ids = motionController.getComponentIds();
-                console.log(xr_ids, 'xr_ids');
-                let triggerComponent = motionController.getComponent(xr_ids[0]);//xr-standard-trigger
-                triggerComponent.onButtonStateChangedObservable.add(() => {
-                    if (triggerComponent.pressed) {
-                        console.log('pressed');
-                    } else {
-                        for (let i = 0; i < roomFullData.length; i++) {
-                            roomFullData[i].actionManager = new ActionManager(scene);
-                            roomFullData[i]?.actionManager?.registerAction(new ExecuteCodeAction(ActionManager.OnPickUpTrigger, function () {
-                                console.log(roomFullData[i].name);
-                                modelLabel(roomFullData[i], modelTexture)
-                            }));
-                        }
-                    }
-                });
-                let squeezeComponent = motionController.getComponent(xr_ids[1]);//xr-standard-squeeze
+                const xr_ids = motionController.getComponentIds()
+                const squeezeComponent = motionController.getComponent(xr_ids[0])
                 squeezeComponent.onButtonStateChangedObservable.add(() => {
                     if (squeezeComponent.pressed) {
-                        console.log('squeezeComponent');
+                        console.log('pressed')
                     } else {
-                        console.log('squeezeComponent');
+                        createModelLabel(roomFullData, scene, modelTexture)
                     }
-                });
-                let thumbstickComponent = motionController.getComponent(xr_ids[2]);//xr-standard-thumbstick
-                thumbstickComponent.onButtonStateChangedObservable.add(() => {
-                    if (thumbstickComponent.pressed) {
-                        console.log('thumbstickComponent');
-                    } else {
-                        console.log('thumbstickComponent');
-                    }
+                })
+                //TODO other controller button events
+                // const squeezeComponent = motionController.getComponent(xr_ids[1])
+                // squeezeComponent.onButtonStateChangedObservable.add(() => {
+                //     if (squeezeComponent.pressed) {
+                //         console.log('squeezeComponent')
+                //     } else {
+                //         console.log('squeezeComponent')
+                //     }
+                // })
+                // const thumbstickComponent = motionController.getComponent(xr_ids[2])
+                // thumbstickComponent.onButtonStateChangedObservable.add(() => {
+                //     if (thumbstickComponent.pressed) {
+                //         console.log('thumbstickComponent')
+                //     } else {
+                //         console.log('thumbstickComponent')
+                //     }
 
-                });
+                // })
 
-                let abuttonComponent = motionController.getComponent(xr_ids[3]);//a-button
-                abuttonComponent.onButtonStateChangedObservable.add(() => {
-                    if (abuttonComponent.pressed) {
-                        console.log('abuttonComponent');
-                    } else {
-                        console.log('abuttonComponent');
-                    }
-                });
-                let bbuttonComponent = motionController.getComponent(xr_ids[4]);//b-button
-                bbuttonComponent.onButtonStateChangedObservable.add(() => {
-                    if (bbuttonComponent.pressed) {
-                        console.log('bbuttonComponent');
+                // const abuttonComponent = motionController.getComponent(xr_ids[3])
+                // abuttonComponent.onButtonStateChangedObservable.add(() => {
+                //     if (abuttonComponent.pressed) {
+                //         console.log('abuttonComponent')
+                //     } else {
+                //         console.log('abuttonComponent')
+                //     }
+                // })
+                // const bbuttonComponent = motionController.getComponent(xr_ids[4])
+                // bbuttonComponent.onButtonStateChangedObservable.add(() => {
+                //     if (bbuttonComponent.pressed) {
+                //         console.log('bbuttonComponent')
 
-                    } else {
-                        console.log('bbuttonComponent');
-                    }
-                });
+                //     } else {
+                //         console.log('bbuttonComponent')
+                //     }
+                // })
             }
         })
-    });
+    })
+}
+
+const createModelLabel = (roomFullData: any, scene:any, modelTexture: any) => {
+    for (let i = 0; i < roomFullData.length; i++) {
+        roomFullData[i].actionManager = new ActionManager(scene)
+        roomFullData[i]?.actionManager?.registerAction(new ExecuteCodeAction(ActionManager.OnPickUpTrigger, function () {
+            console.log(roomFullData[i].name)
+            modelLabel(roomFullData[i], modelTexture)
+        }))
+    }
+}
+
+
+const modelLabel = (model: AbstractMesh, modelTexture: AdvancedDynamicTexture) => {
+    console.log(model, 'model')
+    labelTextSquare(model, modelTexture)
+    labelTargetModel(model, modelTexture)
+    labelLineModel(model, modelTexture)
+}
+
+const textSquare = new Rectangle()
+const labelModel = new TextBlock()
+const labelTextSquare = (model: AbstractMesh, modelTexture: AdvancedDynamicTexture) => {
+    modelTexture.idealWidth = 600
+    textSquare.width = 0.14
+    textSquare.height = "20px"
+    textSquare.cornerRadius = 20
+    textSquare.color = "Orange"
+    textSquare.thickness = 4
+    textSquare.background = "green"
+    modelTexture.addControl(textSquare)
+    textSquare.linkWithMesh(model)
+    textSquare.linkOffsetY = -50
+    if (model) {
+        labelModel.text = model?.name
+    }
+    textSquare.addControl(labelModel)
+}
+
+const targetModel = new Ellipse()
+const labelTargetModel = (model: AbstractMesh, modelTexture: AdvancedDynamicTexture) => {
+    targetModel.width = "7px"
+    targetModel.height = "7px"
+    targetModel.color = "Orange"
+    targetModel.thickness = 2
+    targetModel.background = "green"
+    modelTexture.addControl(targetModel)
+    targetModel.linkWithMesh(model)
+}
+
+const lineModel = new Line()
+const labelLineModel = (model: AbstractMesh, modelTexture: AdvancedDynamicTexture) => {
+    lineModel.lineWidth = 4
+    lineModel.color = "Orange"
+    lineModel.y2 = 10
+    lineModel.linkOffsetY = -3
+    modelTexture.addControl(lineModel)
+    lineModel.linkWithMesh(model)
+    lineModel.connectedControl = textSquare
 }
