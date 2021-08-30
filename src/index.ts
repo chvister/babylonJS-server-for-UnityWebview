9cd2fe64b6ddc47386f67d07c0e66dd4df8b501e
Martin Svoren <msvoro@gmail.com>
2021-08-30 08:43:04 +0200

    lamp simplify

--- src/index.ts
+++ src/index.ts
@@ -49,9 +49,9 @@ const roomModel = SceneLoader.ImportMesh(
         hideObjectByDistance(mesh)
         //LOD simplyfy
         //@ts-ignore
-        mesh[20].optimizeIndices(function() {
+        mesh[40].optimizeIndices(function() {
             //@ts-ignore
-			mesh[20].simplify([{distance:250, quality:0.8}, {distance:300, quality:0.5}, {distance:400, quality:0.3}, {distance:500, quality:0.1}], false, SimplificationType.QUADRATIC, function() {
+			mesh[40].simplify([{distance:1, quality:0.9}, {distance:10, quality: 0.1}], false, SimplificationType.QUADRATIC, function() {
 			console.log("simplification finished");
 		 });
 		})
