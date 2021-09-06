94fc3e98788dbe704eb4c09f60980c440e424755
Martin Svoren <msvoro@gmail.com>
2021-09-06 14:11:08 +0200

    add arena.incremental.babylon

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ import "@babylonjs/loaders/glTF/2.0/glTFLoader"
 const canvas = document.querySelector("#renderCanvas") as HTMLCanvasElement
 const engine = new Engine(canvas, true, undefined, true)
 const scene = new Scene(engine)
-const camera = new FreeCamera("FreeCamera", new Vector3(0, 2, -5), scene);
+const camera = new FreeCamera("FreeCamera", new Vector3(5, 5, 5), scene);
 
 // cameraSettings(scene, canvas)
 // lightSettings(scene)
@@ -67,8 +67,8 @@ engine.runRenderLoop(() => scene.render())
 // )
 // "scene/babylon6/",
 //     "konstrukcia.incremental.babylon",
-SceneLoader.Append("scene/babylon-construction/",
-    "konstrukcia.incremental.babylon",
+SceneLoader.Append("scene/babylon-arena/",
+    "arena.incremental.babylon",
     scene, function (mesh) {
         console.log(mesh.meshes, 'mesh meshes');
         console.log(mesh, 'mesh');
