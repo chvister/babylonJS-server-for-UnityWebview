51b46fa9bf69a4d6b6ac02b2545e3d900e513856
Martin Stolar <martin.stolars@gmail.com>
2021-09-09 11:38:47 +0200

    added global function

--- src/index.ts
+++ src/index.ts
@@ -25,70 +25,44 @@ const canvas = document.querySelector("#renderCanvas") as HTMLCanvasElement
 const engine = new Engine(canvas, true, undefined, true)
 const scene = new Scene(engine)
 // const camera = new FreeCamera("FreeCamera", new Vector3(-11.961, 3.388, -9.275), scene);
-//TODO remove inspector
-//new Inspector()
+
 
 cameraSettings(scene, canvas)
 lightSettings(scene)
 groundSettings(scene)
+//@ts-ignore
+window.newBackgroundColor = (r: float, g: float, b: float, a: float) => {
+    scene.clearColor = new Color4(r, g, b, a);
+}
+
+
 
 window.addEventListener("resize", () => engine.resize())
 engine.runRenderLoop(() => scene.render())
 
-// const environment = scene.createDefaultEnvironment()
 
-//@ts-ignore TODO
-// const xr = await scene.createDefaultXRExperienceAsync({
-//     // xrInput: defaultXRExperience.input,  
-//     // floorMeshes: [environment?.ground] /* Array of meshes to be used as landing points */
-// })
 
-// setupCameraForCollisions(xr.input.xrCamera)
+// SceneLoader.Append("scene/babylon-arena/",
+// "arena-babylon.incremental.babylon",
 
-// const roomModel = SceneLoader.ImportMesh(
-//     "",
-//     "scene/",
-//     "scene1.babylon",
-//     scene,
-//     function (mesh) {
-//         console.log(mesh, 'mesh')
-//         // const modelTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI")
-//         // //@ts-ignore
-//         // scene.activeCamera.attachControl(canvas, true);
-//         // roomModelLabelOnClick(mesh, scene, modelTexture)
+//     scene, function (mesh) {
+//         console.log(mesh.meshes, 'mesh meshes');
+//         console.log(mesh, 'mesh');
+//         //scene.clearColor = new Color4(0, 0, 0, 0)
+//         //@ts-ignore
+//         window.newBackgroundColor = (r: float, g: float, b: float, a: float) => {
+//             scene.clearColor = new Color4(r, g, b, a)
+//         }
 
-//         // // roomModelLabelOnClickXr(mesh, scene, xr, modelTexture)
-//         // // hideObjectByDistance(mesh)
-//         // //LOD simplyfy
-//         // //@ts-ignore
-//         // mesh[105].optimizeIndices(function() {
-//         //     //@ts-ignore
-// 		// 	mesh[105].simplify([{distance:1, quality:0.9}, {distance:10, quality: 0.1}], false, SimplificationType.QUADRATIC, function() {
-// 		// 	console.log("simplification finished");
-// 		//  });
-// 		// })
-//     }
-// )
-
-SceneLoader.Append("scene/babylon-arena/",
-"arena-babylon.incremental.babylon",
-
-    scene, function (mesh) {
-        console.log(mesh.meshes, 'mesh meshes');
-        console.log(mesh, 'mesh');
-        scene.clearColor = new Color4(0, 0, 0, 0)
-
-        const modelTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI")
-        roomModelLabelOnClick(mesh.meshes, scene, modelTexture)
-        scene.removeMesh(mesh.meshes[6])
-        scene.removeMesh(mesh.meshes[2])
-        //@ts-ignore
-        scene.activeCamera.attachControl(canvas, true);
-        // scene.createDefaultCamera(true, true, true);
-        // setTimeout(function () {
-        //     simplifyObjectByDistance(mesh.meshes)
-        // }, 8000);
-        // hideObjectByDistance(mesh.meshes)
-    });
-//TODO remove inspector
-scene.debugLayer.show();
+//         const modelTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI")
+//         roomModelLabelOnClick(mesh.meshes, scene, modelTexture)
+//         scene.removeMesh(mesh.meshes[6])
+//         scene.removeMesh(mesh.meshes[2])
+//         //@ts-ignore
+//         scene.activeCamera.attachControl(canvas, true);
+//         // scene.createDefaultCamera(true, true, true);
+//         // setTimeout(function () {
+//         //     simplifyObjectByDistance(mesh.meshes)
+//         // }, 8000);
+//         // hideObjectByDistance(mesh.meshes)
+//     });
