3edb33aa05978a0a6b1d2e8b61d00632142b8495
Martin Stolar <martin.stolars@gmail.com>
2021-09-13 15:57:42 +0200

    changed xy axies

--- src/index.ts
+++ src/index.ts
@@ -63,7 +63,7 @@ window.newCameraRotation = (x: float, y: float, z: float, w: float) => {
 
 //@ts-ignore
 window.newCameraPosition = (x: float, y: float, z: float) => {
-    camera.position = new Vector3(x, y, z);
+    camera.position = new Vector3(z, y, x);
     console.log(camera.position);
 };
 
